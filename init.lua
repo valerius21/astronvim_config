bb79620caadf9c638315d02676cca10cda505c3e
Valerius Mattfeld <mail@valerius.me>
2023-08-09 22:55:03 +0200

    typescript plugin config

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "catppuccin",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -81,5 +81,6 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    -- vim.api.nvim_create_autocmd("BufWritePre", { command = "TypescriptRemoveUnused" })
   end,
 }
